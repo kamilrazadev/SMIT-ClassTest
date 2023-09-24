d9bf8c80ea22d5b7584746d3e7e11c0448a56f3e
Kamil Raza <m.kamilrazabhatti@gmail.com>
2023-09-24 23:00:14 +0500

    Rersponsiv

---
+++ app.js
@@ -0,0 +1,12 @@
+
+//     let checkAddBtn = false
+
+function handleOpenItem(id){
+//     const addBtn = document.getElementById(id) 
+//     if(checkAddBtn){
+//         addBtn.innerText = '+'
+//     } else {
+//         addBtn.innerText = '-'
+//     }
+//     checkAddBtn = !checkAddBtn
+}
